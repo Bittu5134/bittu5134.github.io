4995efad6f5dea3aa6710899179851d1dac01ae8
Bittu5134 <ssr7a5134@gmail.com>
2026-09-18 03:48:17 +0530

    fix(rss): normalize xml declaration to <?xml version="1.0" encoding="UTF-8"?>

--- scripts/generate-meta.mjs
+++ scripts/generate-meta.mjs
@@ -177,7 +177,7 @@ export async function generateMeta() {
     })
     .join("\n");
 
-  const rssContent = `<?xml version="1.0" encoding="UTF-8" ?>
+  const rssContent = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>Bittu - Systems &amp; Software Blog</title>
